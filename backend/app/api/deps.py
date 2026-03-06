f882ba6b4b43c7599d7baa9855da7aedb486ed22
eranario <ewranario@ucdavis.edu>
2026-03-05 16:11:31 -0800

    removed login page

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,22 +1,11 @@
-import uuid
 from collections.abc import Generator
 from typing import Annotated
 
-import jwt
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from jwt.exceptions import InvalidTokenError
-from pydantic import ValidationError
-from sqlmodel import Session
+from fastapi import Depends, HTTPException
+from sqlmodel import Session, select
 
-from app.core import security
-from app.core.config import settings
 from app.core.db import engine
-from app.models import TokenPayload, User
-
-reusable_oauth2 = OAuth2PasswordBearer(
-    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
-)
+from app.models import User
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -25,25 +14,14 @@ def get_db() -> Generator[Session, None, None]:
 
 
 SessionDep = Annotated[Session, Depends(get_db)]
-TokenDep = Annotated[str, Depends(reusable_oauth2)]
 
 
-def get_current_user(session: SessionDep, token: TokenDep) -> User:
-    try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
-        )
-        token_data = TokenPayload(**payload)
-    except (InvalidTokenError, ValidationError):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
-        )
-    user = session.get(User, uuid.UUID(token_data.sub))
+def get_current_user(session: SessionDep) -> User:
+    user = session.exec(
+        select(User).where(User.is_superuser == True).limit(1)
+    ).first()
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    if not user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=500, detail="No superuser found in database")
     return user
 
 
